71dfda72f3c94797966891d3a0b4037fce2ae74e
cho881020 <kj_cho@idealidea.co.kr>
2020-04-28 22:10:47 +0900

    로그아웃 처리  - 저장된 토큰을 빈칸으로 리셋  - AlertDialog로 의사 확인

--- app/src/main/java/kr/tjeit/apipractice02_get_20200428/MyPageActivity.kt
+++ app/src/main/java/kr/tjeit/apipractice02_get_20200428/MyPageActivity.kt
@@ -1,8 +1,11 @@
 package kr.tjeit.apipractice02_get_20200428
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_my_page.*
+import kr.tjeit.apipractice02_get_20200428.utils.ContextUtil
 import kr.tjeit.apipractice02_get_20200428.utils.GlobalData
 
 class MyPageActivity : BaseActivity() {
@@ -16,6 +19,25 @@ class MyPageActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        logoutBtn.setOnClickListener {
+
+            val alert = AlertDialog.Builder(mContext)
+            alert.setTitle("로그아웃")
+            alert.setMessage("정말 로그아웃 하시겠습니까?")
+            alert.setPositiveButton("확인", {dialog, which ->
+                ContextUtil.setUserToken(mContext, "")
+
+//            다시 로그인 화면으로 이동
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+                finish()
+            })
+            alert.setNegativeButton("취소", null)
+            alert.show()
+
+
+        }
 
     }
 
