c96e270f365995924b53b00ea7da26fe66f1408a
cho881020 <kj_cho@idealidea.co.kr>
2020-04-28 21:34:22 +0900

    토큰 저장 여부에 따라 앱 시작 화면 변경  - Splash액티비티가 진행  - GlobalData의 loginUser가 빈 상태 => 내용 출력 X

--- app/src/main/java/kr/tjeit/apipractice02_get_20200428/SplashActivity.kt
+++ app/src/main/java/kr/tjeit/apipractice02_get_20200428/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import kr.tjeit.apipractice02_get_20200428.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
 
@@ -23,9 +24,22 @@ class SplashActivity : BaseActivity() {
 //        2.5초동안 보여준 뒤 => 로그인 액티비티로 전환
         Handler().postDelayed({
 
-            val myIntent = Intent(mContext, LoginActivity::class.java)
-            startActivity(myIntent)
-            finish()
+            if (ContextUtil.getUserToken(mContext) == "") {
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+                finish()
+            }
+            else {
+
+//                토큰이 저장되어있다면 => 이 토큰으로 사용자 정보를 받아서
+//                GlobalData에 저장 하고 => 액티비티 전환
+
+                val myIntent = Intent(mContext, MyPageActivity::class.java)
+                startActivity(myIntent)
+                finish()
+            }
+
+
 
         }, 2500)
 
