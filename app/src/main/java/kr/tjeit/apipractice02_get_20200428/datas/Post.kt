b123cb2ea7a92816dee8affead0b0e830b76dcae
cho881020 <kj_cho@idealidea.co.kr>
2020-04-29 19:49:15 +0900

    게시글 데이터 클래스 추가

---
+++ app/src/main/java/kr/tjeit/apipractice02_get_20200428/datas/Post.kt
@@ -0,0 +1,13 @@
+package kr.tjeit.apipractice02_get_20200428.datas
+
+import java.util.*
+
+class Post {
+
+    var phoneNum = ""
+    var title = ""
+    var content = ""
+
+    var writer = User()
+    var createdAt = Calendar.getInstance()
+}
